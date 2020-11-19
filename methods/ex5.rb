7b11cb5fa0859956631c98ca85c73c2d7b7cff8c
Sean Richardson <sean.richardson.umd18@gmail.com>
2020-11-19 05:16:18 +0000

    Added solutions to exercises from Methods section

---
+++ methods/ex5.rb
@@ -0,0 +1,9 @@
+#1) Edit the method definition in exercise #4 so that it does print words on the screen. 2) What does it return now?
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end
+
+scream("Yippeee")
+
+#this returns nil now
